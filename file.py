f0a77ad25921da2b7b0e48b8bd9d87add1e80709
ramesh <Dell@DESKTOP-BD2QFT4>
2023-01-29 22:50:47 +0530

    second commit

--- file.py
+++ file.py
@@ -3,3 +3,8 @@ i am fine
 how about you
 yeah going good
 there
+It is the process through which we can store any code
+Developers write code testers write test cases and devops engineers write scripts
+This code we can store and manange in source code management tool
+Different teams can store code simultaneously
+We can retrieve this code at any point of time.
