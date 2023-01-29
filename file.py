6d6812395d7f8bb793bc91b1bfebc2a75176b0b2
Rameshvelu2510 <118846057+Rameshvelu2510@users.noreply.github.com>
2023-01-29 22:53:45 +0530

    Update file.py

--- file.py
+++ file.py
@@ -8,3 +8,7 @@ Developers write code testers write test cases and devops engineers write script
 This code we can store and manange in source code management tool
 Different teams can store code simultaneously
 We can retrieve this code at any point of time.
+Helps in achieving team work
+Can work on different features simultaneously
+Acts like pipeline between onshore and offshore teams
+Track changes (minute level)
